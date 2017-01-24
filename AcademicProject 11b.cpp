e281aa2d19b07f3eb891b841f2f7c050aea54498
Natalia <nataliaaapiekarska@wp.pl>
2017-01-24 21:39:30 +0100

    Add files via upload

---
+++ AcademicProject 11b.cpp
@@ -0,0 +1,30 @@
+//Wczytywaæ znaki a¿ do wczytania dwóch kropek. Wydrukowaæ iloœæ wczytanych znaków ³¹cznie z tymi kropkami.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char znak1, znak2;
+    int licznikZnakow;
+
+    licznikZnakow=0;
+
+    znak1=''
+
+    do
+    {
+        znak2=znak1
+
+        cout << "Prosze podac znak" << endl;
+        cin >> znak1;
+
+        licznikZnakow++;
+
+    } while (znak1!='.'||znak2!='.');
+
+    cout << "Ilosc wszystkich znakow to " << licznikZnakow << endl;
+
+    return 0;
+}
